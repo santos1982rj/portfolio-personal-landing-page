16907eab7b1576e7990098b4fcab1a6e841b7323
santos1982rj <viniciussantosni@outlook.com>
2022-03-03 15:34:15 -0300

    Git Inicial Novo Portfolio

--- js/app.js
+++ js/app.js
@@ -1,14 +1,1 @@
-//Dark Theme
 
-const iconMode = document.getElementById('themeMode');
-const body = document.querySelector('body');
-
-iconMode.addEventListener('click', () => {
-  iconMode.classList.toggle('fa-swap-opacity');
-  body.classList.toggle('dark-theme');
-  if (body.classList.contains('dark-theme')) {
-    iconMode.style.color = 'var(--color-secondary-light)';
-  } else {
-    iconMode.style.color = 'var(--color-primary-dark)';
-  }
-});
