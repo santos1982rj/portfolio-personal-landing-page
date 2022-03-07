33603a7ff2b1dcf51ab4f938f40d7f47611a95d3
santos1982rj <viniciussantosni@outlook.com>
2022-03-07 13:07:15 -0300

    Commit 02

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+const skillBox = document.querySelectorAll('.skills__box');
+const iconBox = document.querySelectorAll('i.icon-box');
+const mouseIn = document.querySelectorAll('.skills__box--mousein');
 
+skillBox.forEach(function (el, index) {
+  el.addEventListener('mouseover', () => {
+    mouseIn[index].classList.toggle('active');
+    iconBox[index].classList.toggle('active');
+  });
+  el.addEventListener('mouseout', () => {
+    mouseIn[index].classList.remove('active');
+    iconBox[index].classList.remove('active');
+  });
+});
+
+// skillBox.forEach(function (el, index) {
+
+// });
